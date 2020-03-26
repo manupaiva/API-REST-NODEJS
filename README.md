b9333e1498489e017d5134e0e5fea81f656c8603
manup15 <manu.paiva73@gmail.com>
2020-03-26 18:30:16 -0300

    install express, body-parser, nodemon

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # API-REST-NODEJS
+
+1 - npm install --save express
+2 - npm install --save body-parser
+3 - npm install --devDependencies nodemon
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict'
  
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+const port = process.env.PORT || 3001
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
 app.listen(3000, () => {
-    console.log('API REST corriendo en http://localhost:3000');
+    console.log(`API REST corriendo en http://localhost:${port}`);
     
 })
