679f24c252e323bd3450ed2ef559458d29805670
manup15 <manu.paiva73@gmail.com>
2020-03-26 20:12:23 -0300

    adding products to shcema with Postman

--- index.js
+++ index.js
@@ -13,11 +13,23 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
 app.get('/api/product', (req, res) => {
-    res.send(200, {products: []})
+    Product.find({}, (err,products) => {
+        if(err) return res.status(500).send({ message: `Error al realizar la peticion: ${err}` })
+        if(!products) return res.status(404).send({ message: `No existen productos` })
+
+        res.send(200, { products })
+    })
 })
 
 app.get('/api/product/:productId', (req, res) => {
-    
+    let productId = req.params.productId
+
+    Product.findById(productId, (err, product) => {
+        if(err) return res.status(500).send({ message: `Error al realizar la peticion: ${err}` })
+        if(!product) return res.status(404).send({ message: `El producto no existe` })
+
+        res.status(200).send({ product })
+    })
 })
 
 app.post('/api/product', (req, res) => {
